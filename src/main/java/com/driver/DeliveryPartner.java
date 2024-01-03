95740425392c13a55b71303576369333ee461a33
Piyush Verma <piyushvrm265@gmail.com>
2024-01-03 12:19:37 +0530

    Changes in Order class

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -1,36 +1,3 @@
-//package com.driver;
-//
-//import com.fasterxml.jackson.annotation.JsonAnyGetter;
-//
-//public class DeliveryPartner {
-//
-//    private String id;
-//    private int numberOfOrders;
-//
-//    public DeliveryPartner(String id) {
-//        this.id = id;
-//    }
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public Integer getNumberOfOrders(){
-//        return numberOfOrders;
-//    }
-//
-//    public void setNumberOfOrders(Integer numberOfOrders) {
-//        this.numberOfOrders = numberOfOrders;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//}
-
-
-
-
 package com.driver;
 
 public class DeliveryPartner {
